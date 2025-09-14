4015c7f5ef48cde978b9cf7132aa29434d3811e6
Grouli123 <Grouli123@yandex.ru>
2025-09-14 17:42:52 +0300

    feat(ui): add HUD with player HP bar and upgrades open control

---
+++ Delta Strike/Assets/Scripts/Player/UI/PlayerHealthBarUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Player.UI
+{
+    public sealed class PlayerHealthBarUI : MonoBehaviour
+    {
+        [SerializeField] private Game.Player.PlayerHealth playerHealth;
+        [SerializeField] private Image fillImage;
+
+        private void Awake()
+        {
+            if (playerHealth != null)
+            {
+                playerHealth.OnChanged += OnHealthChanged;
+                OnHealthChanged(playerHealth.Current, playerHealth.Max);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (playerHealth != null)
+                playerHealth.OnChanged -= OnHealthChanged;
+        }
+
+        private void OnHealthChanged(float current, float max)
+        {
+            if (fillImage != null && max > 0f)
+                fillImage.fillAmount = Mathf.Clamp01(current / max);
+        }
+    }
+}
---
+++ Delta Strike/Assets/Scripts/UI/Upgrade/HudView.cs
@@ -0,0 +1,31 @@
+﻿using Game.Core.DI;
+using UnityEngine;
+
+namespace Game.UI.Upgrade
+{
+    public sealed class HudView : MonoBehaviour
+    {
+        [SerializeField] private GameObject upgradeWindowRoot;
+
+        private Game.Input.IInputService _input;
+
+        private void Awake()
+        {
+            _input = DI.Resolve<Game.Input.IInputService>();
+            if (upgradeWindowRoot != null)
+                upgradeWindowRoot.SetActive(false);
+        }
+
+        public void OnOpenUpgrades()
+        {
+            if (upgradeWindowRoot != null)
+                upgradeWindowRoot.SetActive(true);
+        }
+
+        private void Update()
+        {
+            if (_input.IsOpenUpgradePressed)
+                OnOpenUpgrades();
+        }
+    }
+}
